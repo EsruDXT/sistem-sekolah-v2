85820e50954c77cba11db0c78af3de4f64680f8a
EsruDXT <amengceria2017@gmail.com>
2026-07-22 12:05:29 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Manajemen Siswa
+
+Route::name('students.')->prefix('/students')->group(function () {
+    Route::get('/', StudentController::class, 'index')->name('index');
+
+    Route::get('/{id}', StudentController::class, 'show')->name('show');
+});
